abf1ec00fba25517b6efa7119dd3521bbe780b5e
PC-01\HK <nightmare3685@gmail.com>
2022-09-26 10:11:40 +0900

    全部押しでテストした。

--- HK_SDVX_Controller.ino
+++ HK_SDVX_Controller.ino
@@ -7,18 +7,30 @@ void setup() {
   Serial.begin(9600);
   pinMode(0, INPUT_PULLUP);
 }
+
 void loop() {
   keyfunc();
-  
 }
 
 
 void keyfunc() {
-    Serial.println(!digitalRead(0));
+
+  Serial.println(!digitalRead(0));
+
   if (!digitalRead(0) == HIGH) {
-    NKROKeyboard.add('w');
+    NKROKeyboard.add('s');
+    NKROKeyboard.add('d');
+    NKROKeyboard.add('k');
+    NKROKeyboard.add('l');
+    NKROKeyboard.add('c');
+    NKROKeyboard.add('m');
   } else if (!digitalRead(0) == LOW) {
-    NKROKeyboard.remove('w');
+    NKROKeyboard.remove('s');
+    NKROKeyboard.remove('d');
+    NKROKeyboard.remove('k');
+    NKROKeyboard.remove('l');
+    NKROKeyboard.remove('c');
+    NKROKeyboard.remove('m');
   }
-   NKROKeyboard.send();
+  NKROKeyboard.send();
 }
