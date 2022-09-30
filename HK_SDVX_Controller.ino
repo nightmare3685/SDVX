70be6d17219500174da3852bccdc732aba974eeb
PC-01\HK <nightmare3685@gmail.com>
2022-09-30 11:55:53 +0900

    不要な変数を削除　for文の終了条件を修正

--- HK_SDVX_Controller.ino
+++ HK_SDVX_Controller.ino
@@ -5,23 +5,22 @@
 
 class Button {
 public:
-  char keymap[10] = { 's', 'd', 'k', 'l', 'c', 'm' /*6:Enter 7:ESC*/ };
-  char BT_A = keymap[0], BT_B = keymap[1], BT_C = keymap[2], BT_D = keymap[3], FX_L = keymap[4], FX_R = keymap[5];
+  char keymap[6] = { 's', 'd', 'k', 'l', 'c', 'm' /*6:Enter 7:ESC*/ };
   char VOL_L = 0, VOL_R = 1;
+  char VOL_LL = 'q', VOL_LR = 'w', VOL_RL = 'o', VOL_RR = 'p';
 private:
   ;
 };
 
-volatile byte posision[2];
-
+volatile int posision[2] = { 0, 0 };
 volatile int Arrayright[2] = { 0, 0 };
 volatile int Arrayleft[2] = { 0, 0 };
 Button button;
 
 void setup() {
-  FlexiTimer2::set(5, 1.0 / 1000000, TimerIRQ);  //タイマー割り込み 引数1:時間 引数2:時間の単位 引数3:割り込む関数
-  FlexiTimer2::start();                          //タイマ割込み実行
-  Serial.begin(9600);
+  FlexiTimer2::set(5, 1.0 / 10000000, TimerIRQ);  //タイマー割り込み 引数1:時間 引数2:時間の単位 引数3:割り込む関数
+  FlexiTimer2::start();                           //タイマ割込み実行
+  Serial.begin(115200);
   for (int i = 0; i <= 13; i++) {  //13pinすべてアップ
     pinMode(i, INPUT_PULLUP);
   }
@@ -31,6 +30,8 @@ void loop() {
   keyFunc();
   ReduseValue();
 }
+
+
 void ReduseValue() {
   for (int i = 0; i < (sizeof(Arrayleft) / sizeof(int)); i++) {
 
@@ -47,36 +48,36 @@ void ReduseValue() {
 
 void keyFunc() {
 
-  for (int i = 0; i <  (sizeof(button.keymap) / sizeof(int)); i++) {
+  for (int i = 0; i < (sizeof(button.keymap) / sizeof(char)); i++) {
     if (!digitalRead(i) == HIGH) {
       NKROKeyboard.add(button.keymap[i]);
-    } else if (!digitalRead(i) == LOW) { 
+    } else if (!digitalRead(i) == LOW) {
       NKROKeyboard.remove(button.keymap[i]);
     }
   }
 
   if (Arrayright[button.VOL_L] > 0) {  //左のつまみが右回転
-    NKROKeyboard.add('w');
+    NKROKeyboard.add(button.VOL_LR);
   } else {
-    NKROKeyboard.remove('w');
+    NKROKeyboard.remove(button.VOL_LR);
   }
   if (Arrayleft[button.VOL_L] > 0) {
-    NKROKeyboard.add('q');  //左のつまみが左回転
+    NKROKeyboard.add(button.VOL_LL);  //左のつまみが左回転
   } else {
-    NKROKeyboard.remove('q');
+    NKROKeyboard.remove(button.VOL_LL);
   }
 
 
   if (Arrayright[button.VOL_R] > 0) {  //右のつまみが右回転
-    NKROKeyboard.add('p');
+    NKROKeyboard.add(button.VOL_RR);
   } else {
-    NKROKeyboard.remove('p');
+    NKROKeyboard.remove(button.VOL_RR);
   }
 
   if (Arrayleft[button.VOL_R] > 0) {  //右のつまみが左回転
-    NKROKeyboard.add('o');
+    NKROKeyboard.add(button.VOL_RL);
   } else {
-    NKROKeyboard.remove('o');
+    NKROKeyboard.remove(button.VOL_RL);
   }
 
   NKROKeyboard.send();
@@ -85,8 +86,8 @@ void keyFunc() {
 
 
 void TimerIRQ() {
-  byte currentWave[2];  //最新のロータリー信号
-  byte oldWave[2];      //ひとつ前の信号
+  int currentWave[2];  //最新のロータリー信号
+  int oldWave[2];      //ひとつ前の信号
 
 
   currentWave[button.VOL_L] = (!digitalRead(10) << 1) + !digitalRead(11);  //A相とB相の信号を 000000ABとして変数に代入
@@ -97,7 +98,7 @@ void TimerIRQ() {
     if (currentWave[i] == 3)
       currentWave[i] = 2;
     else if (currentWave[i] == 2)
-      currentWave[i] = 3;                          //回転すると0 1 3 2となるので無理やり2を3、3を2にして0 1 2 3 にする
+      currentWave[i] = 3;                              //回転すると0 1 3 2となるので無理やり2を3、3を2にして0 1 2 3 にする
     oldWave[i] = posision[i] & B00000011;              //pos(curとoldが0000AB A'B'として入ってる変数)と＆演算して000000A'B'だけ代入する
     posision[i] = (oldWave[i] << 2) + currentWave[i];  //回転信号を0000AB A'B'として代入
 
@@ -105,6 +106,7 @@ void TimerIRQ() {
       (currentWave[i] == 0 && oldWave[i] == 1) || (currentWave[i] == 1 && oldWave[i] == 2) || (currentWave[i] == 2 && oldWave[i] == 3) || (currentWave[i] == 3 && oldWave[i] == 0);
     bool isright =
       (currentWave[i] == 0 && oldWave[i] == 3) || (currentWave[i] == 1 && oldWave[i] == 0) || (currentWave[i] == 2 && oldWave[i] == 1) || (currentWave[i] == 3 && oldWave[i] == 2);
+
     if (isleft) {  //反時計回りに回った時
       Arrayleft[i] = 100;
       Arrayright[i] = 0;
